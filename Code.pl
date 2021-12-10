b8a0b1723cc0860c43e93a3ffadd1e2331b6a340
TBersoux <tristanbersoux@gmail.com>
2021-12-10 01:55:25 +0100

    Première partie ok, correction sémantique ok

--- Code.pl
+++ Code.pl
@@ -2,24 +2,19 @@
 
 %Démarrage du programme
 programme :-
-	premiere_etape(Tbox,Abi,Abr).
-	%deuxieme_etape(Abi,Abi1,Tbox),
-	%troisieme_etape(Abi1,Abr).
+	premiere_etape(Tbox,Abi,Abr),
+	deuxieme_etape(Abi,Abi1,Tbox),
+	troisieme_etape(Abi1,Abr).
 
 %Partie 1 - abi : ABox assertions de concept, abi1 : Abox assertions de roles
 
 
 tbox(Tbox) :- findall((C,CG),(equiv(C,CG)),Tbox).
 abi(Abi) :- findall((I,C),(inst(I,C)),Abi).
-abr(Abr) :- findall((I,I2,R),(instR(I,I2,R)),Abir).
-
-
+abr(Abr) :- findall((I,I2,R),(instR(I,I2,R)),Abr).
 
 premiere_etape(Tbox,Abi,Abr):- tbox(Tbox),!,abi(Abi),!,abr(Abr),!.
 	
-
-
-
 % Partie 2
 %====Fourni====
 deuxieme_etape(Abi,Abi1,Tbox) :-
@@ -34,21 +29,45 @@ suite(1,Abi,Abi1,Tbox) :-
 	acquisition_prop_type1(Abi,Abi1,Tbox),!.
 suite(2,Abi,Abi1,Tbox) :-
 	acquisition_prop_type2(Abi,Abi1,Tbox),!.
-suite(R,Abi,Abi1,Tbox) :-
+suite(_,Abi,Abi1,Tbox) :-
 	nl,write('Cette reponse est incorrecte.'),nl,
+	saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
 	
-saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
+lecture([X|L]):-
+read(X),
+X \= fin, !,
+lecture(L).
+lecture([]).
 %=============
 
-concept(anything).
-concept(nothing).
+%Définition du prédicat concept = correction sémantique
+concept(C) :- setof(X,cnamea(X),L),member(C,L). %Concept atomique
+concept(not(C)) :- concept(C). %Négation
+concept(or(C1,C2)) :- concept(C1),concept(C2). %Ou
+concept(and(C1,C2)) :- concept(C1),concept(C2). %Et
+concept(some(R,C)) :- concept(C),setof(X,rname(X),L),member(R,L). %Existe
+concept(all(R,C)) :- concept(C),setof(X,rname(X),L),member(R,L). %Tous
+concept(Ccplx) :- setof(X,cnamena(X),L),member(Ccplx,L). %Concept non-atomique
+concept(I,C) :- setof(X,iname(X),L),member(Instance,L),concept(C). %Instance
 
-lecture([X|L]):-
-	read(X),
-	X \= fin, !,
-	lecture(L).
-lecture([]).
 
+
+%Mise sous nnf
+%====Fourni====
+nnf(not(and(C1,C2)),or(NC1,NC2)):- nnf(not(C1),NC1),
+nnf(not(C2),NC2),!.
+nnf(not(or(C1,C2)),and(NC1,NC2)):- nnf(not(C1),NC1),
+nnf(not(C2),NC2),!.
+nnf(not(all(R,C)),some(R,NC)) :- nnf(not(C),NC),!.
+nnf(not(some(R,C)),all(R,NC)):- nnf(not(C),NC),!.
+nnf(not(not(X)),X):-!.
+nnf(not(X),not(X)):-!.
+nnf(and(C1,C2),and(NC1,NC2)):- nnf(C1,NC1),nnf(C2,NC2),!.
+nnf(or(C1,C2),or(NC1,NC2)):- nnf(C1,NC1), nnf(C2,NC2),!.
+nnf(some(R,C),some(R,NC)):- nnf(C,NC),!.
+nnf(all(R,C),all(R,NC)) :- nnf(C,NC),!.
+nnf(X,X).
+%==============
 
 % Partie 3
 %====Fourni====
